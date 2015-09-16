e0045f2c519d501be5094ad7ee70271e88721762
guodan <gdw117@163.com>
2015-09-16 17:49:44 +0800

    f up

--- Gitdemo/src/com/example/gitdemo/MainActivity.java
+++ Gitdemo/src/com/example/gitdemo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("testing1111");
+        System.out.println("t11111111111esting1111");
     }
 }
